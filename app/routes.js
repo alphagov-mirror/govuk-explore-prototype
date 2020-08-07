0192f3d2b8c2e1f405a950299848a1b17a312b10
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-08-07 22:23:36 +0100

    More browse page bits

--- app/routes.js
+++ app/routes.js
@@ -4,22 +4,13 @@ const router = express.Router()
 const request = require('request');
 // Add your routes here - above the module.exports line
 
-router.get('/topics/:topicSlug', function (req, res) {
+const BASE_URL = "https://govuk-explore-api-p1.herokuapp.com/"
+
+router.get('/browse/:topicSlug', function (req, res) {
   topicSlug = req.params.topicSlug
 
-  request("https://www.gov.uk/api/content/browse/" + topicSlug, { json: true }, (error, result, body) => {
-
-    tp = body.links.children
-    console.log(tp)
-
-    topics = tp.map(function(topic) {
-      return {
-        heading: { text: topic.title },
-        content: { html: "<p class='govuk-body'>" + topic.description + "</p>" }
-      }
-    })
-
-    res.render('topic', { topicSlug: topicSlug, topics: topics })
+  request(BASE_URL + "browse/" + topicSlug, { json: true }, (error, result, body) => {
+    res.render('topic', body)
   })
 })
 
